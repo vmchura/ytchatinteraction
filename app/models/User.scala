d2300f66fe9fd0168b61a321a24cd37e48fe5b91
Vicc <vmchura@gmail.com>
2025-04-14 20:16:13 +0200

    User with identity

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,8 +1,29 @@
 package models
 
+import play.silhouette.api.{Identity, LoginInfo}
 import play.api.libs.json._
 
-case class User(userId: Long, userName: String)
+/**
+ * The user entity.
+ *
+ * @param userId The user ID.
+ * @param userName The username.
+ */
+case class User(userId: Long, userName: String) extends Identity {
+  /**
+   * Gets a login info for this user based on the provided provider ID.
+   *
+   * This helper method simplifies getting a LoginInfo for Silhouette when we need it.
+   * For YouTube OAuth2, the provider ID would be "youtube" and the provider key would be 
+   * the YouTube channel ID, which we can get from a related YtUser.
+   *
+   * @param providerId The provider ID (e.g., "youtube").
+   * @param providerKey The provider key (e.g., YouTube channel ID).
+   * @return A LoginInfo instance.
+   */
+  def loginInfo(providerId: String, providerKey: String): LoginInfo = 
+    LoginInfo(providerId, providerKey)
+}
 
 object User {
   implicit val userFormat: OFormat[User] = Json.format[User]
