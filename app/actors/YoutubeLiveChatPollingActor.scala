16b0cfce0b25d06a49c4f0c3eada9881eb0db05a
Vicc <vmchura@gmail.com>
2025-05-02 16:48:48 +0200

    with formatting

--- app/actors/YoutubeLiveChatPollingActor.scala
+++ app/actors/YoutubeLiveChatPollingActor.scala
@@ -301,7 +301,7 @@ object YoutubeLiveChatPollingActor {
       // Extract the published time
       val publishedAtStr = (message \ "snippet" \ "publishedAt").as[String]
       val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-      val publishedAt = Instant.parse(publishedAtStr)
+      val publishedAt = Instant.from(formatter.parse(publishedAtStr))
 
       // Only process messages that were published after we started monitoring
       if (publishedAt.isAfter(startTime)) {
