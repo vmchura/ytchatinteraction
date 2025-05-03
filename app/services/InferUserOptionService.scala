bd458526e02bceb588f82cbd707fab9b15940d75
Vicc <vmchura@gmail.com>
2025-05-03 16:48:12 +0200

    lowercase

--- app/services/InferUserOptionService.scala
+++ app/services/InferUserOptionService.scala
@@ -16,8 +16,8 @@ import scala.jdk.CollectionConverters.*
 class InferUserOptionService @Inject()(ws: WSClient)(implicit ec: ExecutionContext) {
 
   def inferencePollResponse(eventPoll: EventPoll, options: List[PollOption], response: String): Future[Option[(PollOption, Int)]] = {
-    Future.successful(inferPollResponseWithNLP(eventPoll.pollQuestion, options.map(_.optionText), response)).map{ r =>
-      println(s"${eventPoll.pollQuestion}[${options.map(_.optionText).mkString(",")}]: ${response} => $r")
+    Future.successful(inferPollResponseWithNLP(eventPoll.pollQuestion.toLowerCase, options.map(_.optionText.toLowerCase), response.toLowerCase)).map{ r =>
+      println(s"${eventPoll.pollQuestion}[${options.map(_.optionText).mkString(",")}]: $response => $r")
       r
     }.map{
       case Some((responseOption, confidence)) => options.find(_.optionText.equals(responseOption)).map(po => (po, confidence))
